c052b2246d8154ed0819b20ff2cd6347eb4a8533
WATIS EKTHUVAPRANEE <45465335+watikarosm@users.noreply.github.com>
2021-05-11 15:46:25 -0400

    Add files via upload

---
+++ mainForJSON.js
@@ -0,0 +1,27 @@
+var myCat = {
+    "name": "Spot",
+    "species": "cat",
+    "favFood": "tuna"
+}
+var myFavColors = ["blue", "green", "purple"];
+
+var thePets = [{
+        "name": "Spot",
+        "species": "cat",
+        "favFood": "tuna"
+    },
+    {
+        "name": "Blacktail",
+        "species": "cat",
+        "favFood": "tuna"
+    },
+    {
+        "name": "Shorttail",
+        "species": "cat",
+        "favFood": "tuna"
+    }, {
+        "name": "Longtail",
+        "species": "cat",
+        "favFood": "tuna"
+    }
+];
